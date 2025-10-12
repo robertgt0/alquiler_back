bd7a044c09e2fffefeae477d6c4f03df17aa2e53
Miguel Almanza <almanzaa9928@gmail.com>
2025-10-11 22:41:44 -0400

    Logger winston listo para testear

---
+++ src/modules/notification/utils/logger.ts
@@ -0,0 +1,15 @@
+//T6
+//install winston: npm install winston
+import winston from 'winston';
+
+export const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new winston.transports.Console(),
+    ],
+});
