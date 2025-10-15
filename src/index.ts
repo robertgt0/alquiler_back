01bb56ea947188ffdd0edca3c4427d131a052bae
Mayra <Choquemaqueramayra67@gmail.com>
2025-10-15 15:56:21 -0400

    modificacion del limite de tamaño en express

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,8 @@ connectDB();
 
 // Middlewares globales
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Ruta raíz
 app.get('/', (req: Request, res: Response) => {
