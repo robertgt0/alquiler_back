ce6d39e3cac9651cf102e59dea248ce7c1c03b07
mr_bryanvm <bryan.vasquez.456@gmail.com>
2025-10-18 11:59:57 -0400

    Eliminación del archivo src/modules/notification/routes/notificationRoutes.ts

--- src/modules/notification/routes/notificationRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { createNotification, listNotifications } from '../controllers/notificationController';
-
-const router = Router();
-
-router.post('/', createNotification);
-router.get('/', listNotifications);
-
-export default router;
