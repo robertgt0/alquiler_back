efba6a15a842231a489836be544ece577980cd0f
mr_bryanvm <bryan.vasquez.456@gmail.com>
2025-10-18 11:39:10 -0400

    Modificación de notificationRoutes.ts e implementación de Notification.ts

--- src/modules/notification/routes/notificationRoutes.ts
+++ src/modules/notification/routes/notificationRoutes.ts
@@ -1,30 +1,3 @@
-/*
-Esto deben modificarlo en base a lo que estamos haciendo de notificaciones
-LO QUE ESTA AQUI PARECE SER ALGO GENERICO!!! atte:Adrian
-*/
-/*
-import { Router } from 'express';
-import {
-  getAll,
-  getById,
-  create,
-  update,
-  remove
-} from '../controllers/notificationController';
-import { validateData } from '../middlewares/validate.middleware';
-
-const router = Router();
-
-// Define las rutas de tu módulo aquí
-router.get('/', getAll);
-router.get('/:id', getById);
-router.post('/', validateData, create);
-router.put('/:id', update);
-router.delete('/:id', remove);
-
-export default router;
-*/
-// src/modules/notification/routes/notificationRoutes.ts
 import { Router } from 'express';
 import { createNotification, listNotifications } from '../controllers/notificationController';
 
