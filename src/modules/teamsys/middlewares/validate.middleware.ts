d9c85be106460eaf7f87993f204f3dd40f51b26e
Nicole-Arias <202302132@est.umss.edu>
2025-10-19 14:49:16 -0400

    Update validate.middleware.ts
    teléfono 8 dígitos, nombre ni menos de 2 ni mas de 10 caracteres, verificación de correo.

--- src/modules/teamsys/middlewares/validate.middleware.ts
+++ src/modules/teamsys/middlewares/validate.middleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { CrearUsuarioDto } from '../types';
-import { limpiarInput } from '../utils/validaciones';
+import { limpiarInput, validarPassword, validarCorreoElectronico } from '../utils/validaciones';
 
 export const validateData = (req: Request, res: Response, next: NextFunction): void => {
   const { nombre, correoElectronico, telefono, password, terminosYCondiciones }: CrearUsuarioDto = limpiarInput(req.body)as CrearUsuarioDto;
@@ -10,8 +10,24 @@ export const validateData = (req: Request, res: Response, next: NextFunction): v
     return;
   }
 
+  if (nombre.length < 2) {
+  res.status(400).json({ success: false, message: 'El nombre debe tener al menos 2 caracteres' });
+  return;
+  }
+
+  if (nombre.length > 10) {
+  res.status(400).json({ success: false, message: 'El nombre no puede tener más de 10 caracteres' });
+  return;
+  }
+
   if (!correoElectronico || typeof correoElectronico !== 'string') {
     res.status(400).json({ success: false, message: 'El correo electrónico es requerido y debe ser texto' });
+    return;
+  }
+
+  const correoValido = await validarCorreoElectronico(correoElectronico);
+  if (!correoValido) {
+    res.status(400).json({ success: false, message: 'El correo electrónico no tiene un formato válido' });
     return;
   }
 
@@ -20,10 +36,20 @@ export const validateData = (req: Request, res: Response, next: NextFunction): v
     return;g
   }
 
+  if (!/^\d{8}$/.test(telefono)) {
+    res.status(400).json({ success: false, message: 'El teléfono debe tener exactamente 8 dígitos' });
+    return;
+  }
+
   if (!password || typeof password !== 'string') {
     res.status(400).json({ success: false, message: 'La contraseña es requerida y debe ser texto' });
     return;
   }
+
+  if (!validarPassword(password)) {
+    res.status(400).json({ success: false, message: 'La contraseña no cumple con los requisitos de seguridad' });
+    return;
+  
 
   if (terminosYCondiciones !== true) {
     res.status(400).json({ success: false, message: 'Debes aceptar los términos y condiciones' });
