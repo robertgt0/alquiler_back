56b86cb1a4ccf5f7bd571d8f35b8db0eefc8b738
Hector Abraham Coajera Rocha <hectorabrahamcoajerarocha@gmail.com>
2025-10-22 04:57:59 -0400

    Elinamos src/modules para deployar

--- src/modules/ofertas/routes/ofertas.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as OfertasController from "../controllers/ofertas.controller";
-
-const router = Router();
-
-router.post("/", OfertasController.crearOferta);
-router.get("/", OfertasController.listarOfertas);
-
-export default router;
