8a1cf7ab3f269930398f7b6727e394f71132137f
JulioGarcia <juliocgarcia.go@gmail.com>
2025-11-01 17:13:00 -0400

    Service: se creo un archivo y su respectiva logicapara devolver los detalles de trabajo para un proveedor

---
+++ src/modules/los_vengadores_trabajos/services/cancelar-trabajo.service.ts
@@ -0,0 +1,49 @@
+//src/modules/los_vengadores_trabajos/services/cancelar-trabajo.service.ts
+import Trabajo from "../models/trabajo.model";
+import ProveedorModel from "../models/proveedor.model";
+import ClienteModel from "../models/cliente.model";
+import TrabajoModel from "../models/trabajo.model";
+//import { DetallesTrabajoCanceladoProveedor, DetallesTrabajoCanceladoCliente } from "../types/index";
+
+export class DetallesTrabajo {
+    static async obtenerTrabajoProveedor(TrabajoId: string) {
+        try {
+        const trabajo = await TrabajoModel.findById(
+            TrabajoId,
+            {
+            id_proveedor: 1,
+            fecha: 1,
+            hora_inicio: 1,
+            hora_fin: 1,
+            descripcion_trabajo: 1,
+            costo: 1,
+            estado: 1,
+            }
+        ).populate<{ id_proveedor: { nombre: string } }>(
+            "id_proveedor",
+            "nombre -_id" // solo traer nombre del proveedor
+        );
+
+        if (!trabajo) {
+            return { mensaje: "Trabajo no encontrado" };
+        }
+        return {
+        proveedor: trabajo.id_proveedor.nombre,
+        fecha: (() => {
+            const [año, mes, día] = trabajo.fecha.split("-").map(Number);
+            const fechaObj = new Date(año, mes - 1, día);
+            return new Intl.DateTimeFormat("es-ES", { weekday: "long", day: "numeric", month: "long" })
+                    .format(fechaObj)
+                    .replace(",", "");
+                    })(),
+        horario: `${trabajo.hora_inicio} - ${trabajo.hora_fin}`,
+        descripcion: trabajo.descripcion_trabajo,
+        costo: trabajo.costo,
+        estado: trabajo.estado,
+      };
+        } catch (err) {
+        console.error("Error al obtener detalles del trabajo:", err);
+        return { mensaje: "Error al obtener detalles del trabajo" };
+        }
+    }
+}
