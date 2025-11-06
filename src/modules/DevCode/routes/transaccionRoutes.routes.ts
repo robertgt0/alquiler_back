e7c092658a4f8364eeaf2b660663b98c7fcadbf1
Vargas <alvarovargas6464@gmail.com>
2025-11-06 05:08:55 -0400

    Validacion del AC11, evitando que el ususario pueda editar o eliminar movimientos

--- src/modules/DevCode/routes/transaccionRoutes.routes.ts
+++ src/modules/DevCode/routes/transaccionRoutes.routes.ts
@@ -5,7 +5,9 @@ const router = Router();
 router.post('/', transaccionController.createTransaccion);
 router.get('/', transaccionController.getTransacciones);
 router.get('/:id', transaccionController.getTransaccionById);
+/*
 router.put('/:id', transaccionController.updateTransaccion);
 router.delete('/:id', transaccionController.deleteTransaccion);
+*/
 export default router;
 
