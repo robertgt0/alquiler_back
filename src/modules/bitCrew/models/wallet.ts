a2f66c73c29681ee84b81826bf82144b13b9ab14
TomasMolinaM <tomasmanuelmolinamaldonado79@gmail.com>
2025-11-06 18:24:06 -0400

    feat(ValidarSaldo):BilleteraModelo

--- src/modules/bitCrew/models/wallet.ts
+++ src/modules/bitCrew/models/wallet.ts
@@ -1,28 +1,32 @@
-
 import mongoose, { Schema, model } from 'mongoose';
-
-
 import { IBilletera } from '../types/wallet.types'; 
 
 // --- Esquema Billetera ---
 const BilleteraSchema = new Schema<IBilletera>({
   fixer_id: { 
     type: Schema.Types.ObjectId, 
-    ref: 'Fixer', // Referencia al modelo 'Fixer' de esta misma carpeta
+    ref: 'Fixer',
     required: true, 
-    unique: true 
+    unique: true
   },
   saldo: { 
     type: Number, 
     required: true, 
     default: 0 
   },
+
+  estado: {
+    type: String,
+    enum: ['activo', 'restringido'], // Solo permite estos valores
+    default: 'activo'
+  },
   fecha_actualizacion: { 
     type: Date, 
     default: Date.now 
   }
+
 }, {
-  collection: 'wallet' 
+  collection: 'wallet' // Apunta a la colección 'wallet'
 });
 
 const BilleteraModel = mongoose.models.Billetera || model<IBilletera>('Billetera', BilleteraSchema);
