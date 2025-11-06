89bb5787fd16896985ec937d3b063a2301ba9557
Vargas <alvarovargas6464@gmail.com>
2025-11-05 22:07:20 -0400

    Incorporacion del archivo 'routes/transaccionRoutes.routes.ts'

---
+++ src/modules/DevCode/routes/transaccionRoutes.routes.ts
@@ -0,0 +1,11 @@
+import {Router} from 'express';
+import * as transaccionController from '../controllers/transaccionController.controller';
+
+const router = Router();
+router.post('/', transaccionController.createTransaccion);
+router.get('/', transaccionController.getTransacciones);
+router.get('/:id', transaccionController.getTransaccionById);
+router.put('/:id', transaccionController.updateTransaccion);
+router.delete('/:id', transaccionController.deleteTransaccion);
+export default router;
+
