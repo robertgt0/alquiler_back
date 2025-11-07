053671b9ac1849477bdd8137288233ce87909cf1
JulioGarcia <juliocgarcia.go@gmail.com>
2025-11-06 21:45:45 -0400

    Routes: agregar ruta get  /detalles/cliente/trabajoID para que el cliente vea los detalles de un trabajoç

--- src/modules/los_vengadores_trabajos/routes/trabajo.routes.ts
+++ src/modules/los_vengadores_trabajos/routes/trabajo.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import {crearTrabajoController, obtenerTrabajosController, obtenerTrabajoPorIdController, eliminarTrabajoController,obtenerTrabajoProveedorController} from "../controllers/trabajo.controller";
-import {cancelarTrabajoProveedorController,TerminarTrabajoController} from "../controllers/trabajo.controller";
+import {cancelarTrabajoProveedorController,TerminarTrabajoController,obtenerTrabajoClienteController} from "../controllers/trabajo.controller";
 const router = Router();
 
 
 //obtener detalles del trabajo para el proveedor
 router.get("/detalles/proveedor/:id", obtenerTrabajoProveedorController);
+//obtener detalles del trabajo para el cliente
+router.get("/detalles/cliente/:id", obtenerTrabajoClienteController);
 //cancelar trabajo por el proveedor
 router.put("/cancelar/proveedor/:trabajoId", cancelarTrabajoProveedorController);
 // Ruta para marcar un trabajo como terminado
