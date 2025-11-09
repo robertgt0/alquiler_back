c876cddec57569f95bb4289fc6b58699ffa65105
kevinwcy <kevinchinurijancko@gmail.com>
2025-11-09 19:04:01 -0400

    Kevin Chinuri: Creacion del archivo recarga.model.ts

---
+++ src/modules/DevCode/models/recarga.model.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IRecarga extends Document {
+  nombre: string;
+  detalle: string;
+  monto: number;
+  fecha: Date;
+}
+
+const RecargaSchema = new Schema<IRecarga>({
+  nombre: { type: String, required: true },
+  detalle: { type: String, required: true },
+  monto: { type: Number, required: true },
+  fecha: { type: Date, default: Date.now },
+});
+
+const Recarga = mongoose.model<IRecarga>("Recarga", RecargaSchema);
+export default Recarga;
